dffc4546a19f6863cd9a0b3eefa553e735135f9e
doubiliu <liumengyu@ngd.neo.org>
2022-04-22 10:50:46 +0800

    change name

--- src/Neo.Cryptography.BLS12_381/Bls12381Wrapper.cs
+++ src/Neo.Cryptography.BLS12_381/Bls12381Wrapper.cs
@@ -18,43 +18,43 @@ namespace Neo.Cryptography.BLS12_381
     /// </summary>
     public static class Bls12381Wrapper
     {
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr gt_add(IntPtr gt1, IntPtr gt2);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr gt_mul(IntPtr gt, UInt64 multi);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr gt_neg(IntPtr gt);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g1_add(IntPtr g1_1, IntPtr g1_2);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g1_mul(IntPtr g1, UInt64 multi);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g1_neg(IntPtr g1);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g2_add(IntPtr g2_1, IntPtr g2_2);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g2_mul(IntPtr g2, UInt64 multi);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g2_neg(IntPtr g2);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern void gt_dispose(IntPtr rawPtr);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern void g1_dispose(IntPtr rawPtr);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern void g2_dispose(IntPtr rawPtr);
 
-        [DllImport("bls12381", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("Neo_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g1_g2_pairing(IntPtr g1, IntPtr g2);
 
         /// <summary>
