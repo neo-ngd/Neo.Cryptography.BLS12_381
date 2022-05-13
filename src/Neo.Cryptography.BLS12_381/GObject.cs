ae8d2cb11ee36e2ab019accac1e949ca8fa4859e
Erik Zhang <erik@neo.org>
2022-05-13 12:53:45 +0800

    Renaming

--- src/Neo.Cryptography.BLS12_381/GObject.cs
+++ src/Neo.Cryptography.BLS12_381/GObject.cs
@@ -10,103 +10,90 @@ namespace Neo.Cryptography.BLS12_381
         private const int Gt = 576;
 
         private readonly IntPtr ptr;
-        private readonly int type;
+        private readonly int size;
 
-        private GObject(IntPtr ptr, int t)
+        private GObject(IntPtr ptr, int size)
         {
             this.ptr = ptr;
-            this.type = t;
+            this.size = size;
         }
 
         public GObject(byte[] g)
         {
-            int len = g.Length;
-            if (len is G1 or G2 or Gt)
+            size = g.Length;
+            if (size is G1 or G2 or Gt)
             {
-                IntPtr tmp = Marshal.AllocHGlobal(len);
-                Marshal.Copy(g, 0, tmp, len);
-                this.type = len;
-                this.ptr = tmp;
+                ptr = Marshal.AllocHGlobal(size);
+                Marshal.Copy(g, 0, ptr, size);
             }
-            else throw new Exception($"Bls12381 operation falut,type:format,error:valid point length");
+            else throw new Exception($"Bls12381 operation fault, type:format, error:valid point length");
         }
 
         ~GObject()
         {
-            try
+            switch (size)
             {
-                switch (type)
-                {
-                    case G1:
-                        Interop.g1_dispose(ptr);
-                        break;
-                    case G2:
-                        Interop.g2_dispose(ptr);
-                        break;
-                    case Gt:
-                        Interop.gt_dispose(ptr);
-                        break;
-                    default:
-                        throw new Exception($"Bls12381 operation fault, type:format, error:type mismatch");
-                }
-            }
-            catch (Exception)
-            {
-                throw new Exception($"Bls12381 operation falut,type:format,error:dispose failed");
+                case G1:
+                    Interop.g1_dispose(ptr);
+                    break;
+                case G2:
+                    Interop.g2_dispose(ptr);
+                    break;
+                case Gt:
+                    Interop.gt_dispose(ptr);
+                    break;
             }
         }
 
         public GObject Neg()
         {
-            GObject G = type switch
+            return size switch
             {
                 G1 => new GObject(Interop.g1_neg(ptr), G1),
                 G2 => new GObject(Interop.g2_neg(ptr), G2),
                 Gt => new GObject(Interop.gt_neg(ptr), Gt),
                 _ => throw new Exception($"Bls12381 operation fault, type:format, error:valid point length")
             };
-            return G;
         }
 
         public static GObject Add(GObject p1, GObject p2)
         {
-            if (p1.type != p2.type)
+            if (p1.size != p2.size)
             {
                 throw new Exception($"Bls12381 operation fault, type:format, error:type mismatch");
             }
-            return p1.type switch
+            return p1.size switch
             {
                 G1 => new GObject(Interop.g1_add(p1.ptr, p2.ptr), G1),
                 G2 => new GObject(Interop.g2_add(p1.ptr, p2.ptr), G2),
                 Gt => new GObject(Interop.gt_add(p1.ptr, p2.ptr), Gt),
-                _ => throw new Exception($"Bls12381 operation fault,type:format,error:valid point length")
+                _ => throw new Exception($"Bls12381 operation fault, type:format, error:valid point length")
             };
         }
 
         public static GObject Mul(GObject p, ulong x)
         {
-            return p.type switch
+            return p.size switch
             {
                 G1 => new GObject(Interop.g1_mul(p.ptr, x), G1),
                 G2 => new GObject(Interop.g2_mul(p.ptr, x), G2),
                 Gt => new GObject(Interop.gt_mul(p.ptr, x), Gt),
-                _ => throw new Exception($"Bls12381 operation falut,type:format,error:valid point length")
+                _ => throw new Exception($"Bls12381 operation fault, type:format, error:valid point length")
             };
         }
 
         public static GObject Pairing(GObject p1, GObject p2)
         {
-            if (p1.type != G1 || p2.type != G2)
+            if (p1.size != G1 || p2.size != G2)
             {
                 throw new Exception($"Bls12381 operation fault, type:format, error:type mismatch");
             }
-            GObject gt = new(Interop.g1_g2_pairing(p1.ptr, p2.ptr), Gt);
-            return gt;
+            return new(Interop.g1_g2_pairing(p1.ptr, p2.ptr), Gt);
         }
 
         public byte[] ToByteArray()
         {
-            byte[] buffer = GC.AllocateUninitializedArray<byte>(type);
+            byte[] buffer = GC.AllocateUninitializedArray<byte>(size);
             Marshal.Copy(ptr, buffer, 0, buffer.Length);
             return buffer;
         }
--- src/Neo.Cryptography.BLS12_381/Interop.cs
+++ src/Neo.Cryptography.BLS12_381/Interop.cs
@@ -9,7 +9,7 @@ namespace Neo.Cryptography.BLS12_381
         public static extern IntPtr gt_add(IntPtr gt1, IntPtr gt2);
 
         [DllImport("Neo_Cryptography_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr gt_mul(IntPtr gt, UInt64 multi);
+        public static extern IntPtr gt_mul(IntPtr gt, ulong multi);
 
         [DllImport("Neo_Cryptography_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr gt_neg(IntPtr gt);
@@ -18,7 +18,7 @@ namespace Neo.Cryptography.BLS12_381
         public static extern IntPtr g1_add(IntPtr g1_1, IntPtr g1_2);
 
         [DllImport("Neo_Cryptography_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr g1_mul(IntPtr g1, UInt64 multi);
+        public static extern IntPtr g1_mul(IntPtr g1, ulong multi);
 
         [DllImport("Neo_Cryptography_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g1_neg(IntPtr g1);
@@ -27,7 +27,7 @@ namespace Neo.Cryptography.BLS12_381
         public static extern IntPtr g2_add(IntPtr g2_1, IntPtr g2_2);
 
         [DllImport("Neo_Cryptography_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr g2_mul(IntPtr g2, UInt64 multi);
+        public static extern IntPtr g2_mul(IntPtr g2, ulong multi);
 
         [DllImport("Neo_Cryptography_BLS12_381_Native", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr g2_neg(IntPtr g2);
